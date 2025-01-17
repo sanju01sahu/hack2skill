ab3b01f31e173c96d4606a1ca782d5fd49b33b57
Sanjay <sanju01.sahu@gmail.com>
2025-01-17 12:35:16 +0530

    frontend: second push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+let iniState = {
+  
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const onClick = (e)=>{
+    
+  }
+
+  const onFormSubmit = (e)=>{
+    
+  }
 
   return (
     <div className='app'>
-      hello
+      <div className='nav' >
+        <div><img src="./assets/logo1.png" alt="" /></div>
+        <div><button>Register</button></div>
+      </div>
+      <div className='section-one' >
+        <div>
+          <h2>Integration for H2S Hackathon 2025</h2>
+          <img src="./assets/img1.png" alt="" />
+        </div>
+        <div className='register-form'>
+          <form>
+            <h1>Build with H2S to win $14000</h1>
+            <label htmlFor="name">Full Name</label>
+            <input type="text" id='name' name='name' className='name' placeholder='Enter your full name here' />
+            <label htmlFor="mobile">Contact Number</label>
+            <input type="number" id='mobile' name='mobile' className='mobile' placeholder='Enter your full name here' />
+            <label htmlFor="country">Select Your Country</label>
+            <input type="text" id='country' name='country' className='country' placeholder='Enter your full name here' />
+            <label htmlFor="email">Email ID</label>
+            <input type="email" id='email' name='email' className='email' placeholder='Enter your full name here' />
+            <label htmlFor="name">Company Name</label>
+            <input type="text" id='company' name='company' className='company' placeholder='Enter your full name here' />
+
+            <button  >Register</button>
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
